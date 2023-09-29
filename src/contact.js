cdf69e222b2ee2a5cb8ea66c260e54b7aecc6bbf
Grandeman <joshuajmorales01@gmail.com>
2023-09-29 00:19:27 -0400

    Style contact page

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,19 @@
 export function createContactPage() {
     const contactDiv = document.createElement('div');
-    contactDiv.textContent = "Visit us at: 123 Main Street, City, State ZIP. For reservations, call: 123-456-7890";
+    contactDiv.id = 'contact-content';
+    
+    const contactInfo = document.createElement('div');
+    contactInfo.classList.add('contact-info');
+    const address = document.createElement('p');
+    address.classList.add('address');
+    address.textContent = "Visit us at: 123 Main Street, City, State ZIP.";
+    contactInfo.appendChild(address);
+    const telephone = document.createElement('p');
+    telephone.classList.add('telephone');
+    telephone.textContent =  `For reservations,call: 123-456-7890`;
+    contactInfo.appendChild(telephone);
+    
+    contactDiv.appendChild(contactInfo);
+
     return contactDiv;
 }
